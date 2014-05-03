64df40be29744d7e9f34917e15551c5b13505c61
jrichcs3450 <joseph.richardson@aggiemail.usu.edu>
2014-05-02 23:46:35 -0600

    Fixed the no-repeats permutator in GameManager.  EVERYTHING _SHOULD_ be working now!

--- src/TrueMain.cpp
+++ src/TrueMain.cpp
@@ -50,12 +50,12 @@ int main(int argc, char** argv)
 
         //prepare tasks to run
         std::vector<StrategyEnum::StrategyEnum> strategies;
-        strategies.push_back(StrategyEnum::HumanControlledStrategy);
-        strategies.push_back(StrategyEnum::AggressiveStrategy);
-		strategies.push_back(StrategyEnum::ObtainSmallestContinentsFirstStrategy);
-		////strategies.push_back(StrategyEnum::PacifistStrategy);
-		////strategies.push_back(StrategyEnum::ImmediateBestValueStrategy1);
-		////strategies.push_back(StrategyEnum::PreyOnWeakStrategy);
+        ////strategies.push_back(StrategyEnum::HumanControlledStrategy);
+        strategies.push_back(StrategyEnum::ObtainSmallestContinentsFirstStrategy);
+		strategies.push_back(StrategyEnum::AggressiveStrategy);
+		strategies.push_back(StrategyEnum::PreyOnWeakStrategy);
+		strategies.push_back(StrategyEnum::ImmediateBestValueStrategy1);
+		strategies.push_back(StrategyEnum::PacifistStrategy);
 		///strategies.push_back(StrategyEnum::BadStrategy1);
 
         std::cout << "Strategies being used: ";
@@ -77,17 +77,9 @@ int main(int argc, char** argv)
         }
         std::cout << std::endl;
 
-        std::vector<GameTask> tasksToRun = GameManager::getNonrepeatingPermutationsFor(strategies, maps, numberOfPlayers, 1);//timesToRepeatEachGame);
+        std::vector<GameTask> tasksToRun = GameManager::getNonrepeatingPermutationsFor(strategies, maps, numberOfPlayers, timesToRepeatEachGame);
 
-		for (int i=0; i<tasksToRun.size(); i++) {
-			for (int j=0; j<6; j++)
-				std::cout<<tasksToRun[i].players[j]<<" ";
-			std::cout<<std::endl;
-		}
-
-		
-		
-        //start the manager
+		//start the manager
         GameManager manager(totalProcCount - 1);
         manager.setGamesToRun(tasksToRun);
         ////manager.readInExistingReport("reportLocation");
