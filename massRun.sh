7203945ca95aa4eb7705a6ff33058aedb4ac0f19
Jacob Meline <Jacob.meline@gmail.com>
2014-05-02 20:38:10 -0600

    Pushing stats for Aggressive Player

--- massRun.sh
+++ massRun.sh
@@ -1,7 +1,12 @@
 #/usr/bin/bash
 
-mpiexec -np 3 risk_exec data/orderAdvantage/smallestFirst2_10000.txt 10000 2 > /dev/null;
-mpiexec -np 3 risk_exec data/orderAdvantage/smallestFirst3_10000.txt 10000 3 > /dev/null;
-mpiexec -np 3 risk_exec data/orderAdvantage/smallestFirst4_10000.txt 10000 4 > /dev/null;
-mpiexec -np 3 risk_exec data/orderAdvantage/smallestFirst5_10000.txt 10000 5 > /dev/null;
-mpiexec -np 3 risk_exec data/orderAdvantage/smallestFirst6_10000.txt 10000 6 > /dev/null;
+mpiexec -np 3 risk_exec data/orderAdvantage/aggressive2_10000.txt 10000 2 > /dev/null;
+echo done 2
+mpiexec -np 3 risk_exec data/orderAdvantage/aggressive3_10000.txt 10000 3 > /dev/null;
+echo done 3
+mpiexec -np 3 risk_exec data/orderAdvantage/aggressive4_10000.txt 10000 4 > /dev/null;
+echo done 4
+mpiexec -np 3 risk_exec data/orderAdvantage/aggressive5_10000.txt 10000 5 > /dev/null;
+echo done 5
+mpiexec -np 3 risk_exec data/orderAdvantage/aggressive6_10000.txt 10000 6 > /dev/null;
+echo done 6
