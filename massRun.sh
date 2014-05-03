3fa40d004e444f21fb887a6dbb8ae46c8de74c83
Jacob Meline <Jacob.meline@gmail.com>
2014-05-02 22:13:04 -0600

    Pusing smallestContinent data to repo

--- massRun.sh
+++ massRun.sh
@@ -1,12 +1,14 @@
 #/usr/bin/bash
 
-mpiexec -np 3 risk_exec data/orderAdvantage/aggressive2_10000.txt 10000 2 > /dev/null;
+FILE="smallestContinentFirst"
+
+mpiexec -np 3 risk_exec data/orderAdvantage/${FILE}2_10000.txt 10000 2 > /dev/null;
 echo done 2
-mpiexec -np 3 risk_exec data/orderAdvantage/aggressive3_10000.txt 10000 3 > /dev/null;
+mpiexec -np 3 risk_exec data/orderAdvantage/${FILE}3_10000.txt 10000 3 > /dev/null;
 echo done 3
-mpiexec -np 3 risk_exec data/orderAdvantage/aggressive4_10000.txt 10000 4 > /dev/null;
+mpiexec -np 3 risk_exec data/orderAdvantage/${FILE}4_10000.txt 10000 4 > /dev/null;
 echo done 4
-mpiexec -np 3 risk_exec data/orderAdvantage/aggressive5_10000.txt 10000 5 > /dev/null;
+mpiexec -np 3 risk_exec data/orderAdvantage/${FILE}5_10000.txt 10000 5 > /dev/null;
 echo done 5
-mpiexec -np 3 risk_exec data/orderAdvantage/aggressive6_10000.txt 10000 6 > /dev/null;
+mpiexec -np 3 risk_exec data/orderAdvantage/${FILE}6_10000.txt 10000 6 > /dev/null;
 echo done 6
